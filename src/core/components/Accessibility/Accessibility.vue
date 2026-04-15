b9bbeb0bd1b6f0e33e110f39bac628dc8ec1cc70
Ana Santos <ana.katarine@mdh.gov.br>
2026-04-15 10:24:28 -0300

    Adiciona botão de reset de tamanho de fonte e substitui ícone FontAwesome por SVG no menu de acessibilidade

--- src/core/components/Accessibility/Accessibility.vue
+++ src/core/components/Accessibility/Accessibility.vue
@@ -2,7 +2,7 @@
   <Card custom-class="menu" v-if="acessibilidade">
     <div>
       <p class="m-1 p-0 item-titulo">Texto</p>
-      <div class="menu-section">
+      <div class="menu-section mb-2">
         <button
           class="br-button item-menu font-acessibilidade"
           type="button"
@@ -22,6 +22,18 @@
         >
           A-
           <p class="m-0 texto-menu">Texto menor</p>
+        </button>
+      </div>
+      <div class="menu-section">
+        <button
+          class="br-button item-menu font-acessibilidade"
+          type="button"
+          aria-label="Tamanho padrão de fonte"
+          title="Tamanho padrão de fonte"
+          @click="resetFontSize"
+        >
+          A
+          <p class="m-0 texto-menu">Tamanho padrão</p>
         </button>
       </div>
     </div>
@@ -51,7 +63,12 @@
       title="Abrir menu de acessibilidade"
       @click="toggleAcessibilidade"
     >
-      <i class="fas fa-universal-access icon-accessibility" aria-hidden="true"></i>
+      <!-- <i class="fas fa-universal-access icon-accessibility" aria-hidden="true"></i> -->
+      <svg class="icon-accessibility" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path
+          d="M256 48c114.953 0 208 93.029 208 208 0 114.953-93.029 208-208 208-114.953 0-208-93.029-208-208 0-114.953 93.029-208 208-208m0-40C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 56C149.961 64 64 149.961 64 256s85.961 192 192 192 192-85.961 192-192S362.039 64 256 64zm0 44c19.882 0 36 16.118 36 36s-16.118 36-36 36-36-16.118-36-36 16.118-36 36-36zm117.741 98.023c-28.712 6.779-55.511 12.748-82.14 15.807.851 101.023 12.306 123.052 25.037 155.621 3.617 9.26-.957 19.698-10.217 23.315-9.261 3.617-19.699-.957-23.316-10.217-8.705-22.308-17.086-40.636-22.261-78.549h-9.686c-5.167 37.851-13.534 56.208-22.262 78.549-3.615 9.255-14.05 13.836-23.315 10.217-9.26-3.617-13.834-14.056-10.217-23.315 12.713-32.541 24.185-54.541 25.037-155.621-26.629-3.058-53.428-9.027-82.141-15.807-8.6-2.031-13.926-10.648-11.895-19.249s10.647-13.926 19.249-11.895c96.686 22.829 124.283 22.783 220.775 0 8.599-2.03 17.218 3.294 19.249 11.895 2.029 8.601-3.297 17.219-11.897 19.249z"
+        />
+      </svg>
     </button>
     <div class="game-bar" :class="{ fixed: props.fixed }">
       <!-- <button class="br-button circle small" type="button"
@@ -109,11 +126,17 @@ const toggleAcessibilidade = () => {
   font-size: 18px;
 }
 
+.icon-accessibility {
+  width: 1.25rem;
+  height: 1.25rem;
+  fill: currentColor;
+}
+
 .menu {
   position: fixed;
   z-index: 9999 !important;
-  bottom: 12rem;
-  right: 6rem;
+  bottom: 9rem;
+  right: 5rem;
   width: 300px;
 }
 
@@ -170,7 +193,7 @@ const toggleAcessibilidade = () => {
 
 @media (max-width: 768px) {
   .menu {
-    right: 3rem;
+    right: 5rem;
   }
 }
 
