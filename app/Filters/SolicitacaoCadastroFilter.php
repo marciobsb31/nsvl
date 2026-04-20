6d25633619b44e683f98ad31a392c2278314f3fe
Daniel Landim <daniel.landim@mdh.gov.br>
2026-04-20 11:49:19 -0300

    ajutes filtro solicitacao de cadastro

--- app/Filters/SolicitacaoCadastroFilter.php
+++ app/Filters/SolicitacaoCadastroFilter.php
@@ -9,16 +9,11 @@ class SolicitacaoCadastroFilter extends Filters
     protected array $filters = [
         'nome',
         'cpf',
-        'esfera_id',
-        'estado_id',
+        'uf_id',
         'municipio_id',
-        'cargo',
         'orgao',
-        'status_solicitacao_id',
-        'uf',
-        'municipio',
-        'esfera',
-        'status',
+        'esfera_id',
+        'status_id',
     ];
 
     public function nome(string $param)
@@ -43,7 +38,7 @@ class SolicitacaoCadastroFilter extends Filters
         $this->builder->where('esfera_id', $param);
     }
 
-    public function estadoId(string $param)
+    public function ufId(string $param)
     {
         $param = Helpers::onlyDigits($param);
         $this->builder->where('uf_id', $param);
@@ -55,76 +50,14 @@ class SolicitacaoCadastroFilter extends Filters
         $this->builder->where('municipio_id', $param);
     }
 
-    public function cargo(string $param)
-    {
-        $this->builder->where('cargo', 'ILIKE', '%'.$param.'%');
-    }
-
     public function orgao(string $param)
     {
         $this->builder->where('orgao', 'ILIKE', '%'.$param.'%');
     }
 
-    public function statusSolicitacaoId(string $param)
+    public function statusId(string $param)
     {
         $param = Helpers::onlyDigits($param);
         $this->builder->where('status_id', $param);
-    }
-
-    public function uf(string $param)
-    {
-        $digits = Helpers::onlyDigits($param);
-        if ($digits !== '') {
-            $this->builder->where('uf_id', $digits);
-
-            return;
-        }
-
-        $uf = strtoupper(trim($param));
-        $this->builder->whereHas('ufRelacao', function ($query) use ($uf) {
-            $query->where('sigla', $uf);
-        });
-    }
-
-    public function municipio(string $param)
-    {
-        $digits = Helpers::onlyDigits($param);
-        if ($digits !== '') {
-            $this->builder->where('municipio_id', $digits);
-
-            return;
-        }
-
-        $this->builder->whereHas('municipioRelacao', function ($query) use ($param) {
-            $query->where('nome', 'ILIKE', '%'.$param.'%');
-        });
-    }
-
-    public function esfera(string $param)
-    {
-        $digits = Helpers::onlyDigits($param);
-        if ($digits !== '') {
-            $this->builder->where('esfera_id', $digits);
-
-            return;
-        }
-
-        $this->builder->whereHas('esfera', function ($query) use ($param) {
-            $query->whereRaw('LOWER(nome) = ?', [mb_strtolower(trim($param))]);
-        });
-    }
-
-    public function status(string $param)
-    {
-        $digits = Helpers::onlyDigits($param);
-        if ($digits !== '') {
-            $this->builder->where('status_id', $digits);
-
-            return;
-        }
-
-        $this->builder->whereHas('statusSolicitacao', function ($query) use ($param) {
-            $query->whereRaw('LOWER(nome) = ?', [mb_strtolower(trim($param))]);
-        });
     }
 }
